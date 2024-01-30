63b7f3bf8c8ca8b029ef8d4ca6be0c0caaaf0223
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2024-01-29 22:42:23 -0500

    Time: 0 ms (100.00%) | Memory: 45.3 MB (50.54%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,9 +1,15 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int plantingSpaces = 0;
+        int plantingSpaces = 0;//The number of spaces available
 
+        //Iterates through the array to find the number of available spaces
         for(int i = 0; i < flowerbed.length; i++)
         {
+            /*
+            If there is a flower planted, skip the adjacent place.
+            Else check if the next spot is planted. If yes, skip
+            Else increment the plantingSpaces and skip th adjacent spot.
+            */
             if(flowerbed[i] == 1)
             {
                 i++;
@@ -19,6 +25,7 @@ class Solution {
             }
         }
 
+        //Checks if the plantingSpaces are equal to or greater than n
         if(plantingSpaces >= n)
         {
             return true;
