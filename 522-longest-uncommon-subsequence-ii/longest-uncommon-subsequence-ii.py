16173416383ec4bdde0a296af80300cb60ba712f
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2024-12-26 01:17:25 -0500

    Time: 3 ms (47.69%) | Memory: 18 MB (6.33%) - LeetSync

---
+++ 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.py
@@ -0,0 +1,72 @@
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        strs.sort(reverse=True, key=self.sort)
+        
+        rejected = []
+        selected = []
+        print(strs)
+
+        for i in range(len(strs)):
+            if i == 0:
+                selected.append(strs[i])
+            elif self.validate(selected, rejected, strs[i]):
+                if selected != [] and len(strs[i]) > len(selected[0]):
+                    rejected.extend(selected)
+                    selected.clear()
+                    selected.append(strs[i])
+                else:
+                    selected.append(strs[i])
+            else:
+                for x in selected:
+                    if x == strs[i]:
+                        selected.remove(strs[i])
+                        break
+                rejected.append(strs[i])
+                
+                
+        print(rejected)
+        print(selected)
+
+        if selected == []:
+            return -1
+        else:
+            return len(selected[0])
+            
+    def sort(self, e):
+        return len(e)
+
+
+    def validate(self, selected: str, rejected: List[str], s: str) -> bool:
+        for x in selected:
+            if self.isSubsequence(x, s):
+                return False
+
+        for x in rejected:
+            if self.isSubsequence(x, s):
+                return False
+
+        return True
+
+    def isSubsequence(self, a: str, b: str) -> bool:
+        if a == b:
+            return True
+
+        aLen = len(a)
+        bLen = len(b)
+
+        if bLen > aLen:
+            return False
+
+        aCount = 0
+        bCount = 0
+
+        while(aCount < aLen and bCount < bLen):
+            if a[aCount] == b[bCount]:
+                bCount += 1
+
+            aCount += 1
+
+        if bCount == bLen:
+            return True
+        else:
+            return False
