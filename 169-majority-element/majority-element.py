4b19b26d096ee0e895d3f99473750398a475a6e5
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2025-01-07 21:36:57 -0500

    Time: 8 ms (40.31%) | Memory: 19.3 MB (18.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         table = Counter(nums)
-        print(table)
         vals = max(table.values())
         
         for x,y in table.items():
