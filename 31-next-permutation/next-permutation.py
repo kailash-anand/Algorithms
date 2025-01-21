0c72d72138668618bfe813f85ec8d91a50acf686
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2025-01-20 23:12:40 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (8.14%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,40 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        minimum = 0
+        pivot = 0
+
+        for i in reversed(range(1, length)):
+            if nums[i - 1] < nums[i]:
+                pivot = i - 1
+                break
+
+        minimum = pivot + 1
+
+        for i in reversed(range(1, length)):
+            if nums[i - 1] < nums[i]:
+                self.swap(i-1, minimum, nums)
+                self.sortSelective(nums[i:length], nums)
+                return
+            else:
+                if nums[i] < nums[minimum] and nums[i] > nums[pivot]:
+                    minimum = i
+
+        nums.sort()
+
+    def swap(self, num1, num2, list):
+        temp = list[num1]
+        list[num1] = list[num2]
+        list[num2] = temp
+
+    def sortSelective(self, toSort, toAdd):
+        print(toSort)
+        toSort.sort()
+
+        start = len(toAdd) - len(toSort)
+        end = len(toAdd)
+        
+        toAdd[start:end] = toSort
