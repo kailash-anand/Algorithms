3746f2a21499dbcdf3e6a0e986aab64c524d98cd
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2026-01-07 00:55:23 -0500

    Time: 1 ms (28.18%) | Memory: 19.4 MB (16.03%) - LeetSync

--- 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.py
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.py
@@ -1,12 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        all_equal = True
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                all_equal = False
-
-        if all_equal:
-            return 0
-        else:
+        if len(set(nums)) != 1:
             return 1
+
+        return 0
