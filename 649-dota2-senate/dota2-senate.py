656ccd0f50c544be410fa0b4fc03aa2e578e5dcc
Kailash Anand <115104604+kailash-anand@users.noreply.github.com>
2026-01-26 20:07:15 -0500

    Time: 15 ms (41.09%) | Memory: 19.4 MB (29.52%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -3,13 +3,21 @@ class Solution:
         from collections import deque
 
         voterQueue = deque(senate)
+        Rcount, Dcount = 0, 0
         banDcount, banRcount = 0, 0
 
-        while len(set(voterQueue)) > 1:
+        for x in senate:
+            if x == "D":
+                Dcount += 1
+            else:
+                Rcount += 1
+
+        while Rcount > 0 and Dcount > 0:
             if voterQueue[0] == "R":
                 if banRcount > 0:
                     voterQueue.popleft()
                     banRcount -= 1
+                    Rcount -= 1
                 else:
                     banDcount += 1
                     voterQueue.append(voterQueue.popleft())
@@ -17,6 +25,7 @@ class Solution:
                 if banDcount > 0:
                     voterQueue.popleft()
                     banDcount -= 1
+                    Dcount -= 1
                 else:
                     banRcount += 1
                     voterQueue.append(voterQueue.popleft())
